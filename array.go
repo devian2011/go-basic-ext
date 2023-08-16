392174bf80efa66a7e0f59040b494f741eeef0f3
devian2011 <romanov.i.u@yandex.ru>
2023-08-16 23:00:09 +0300

    Append array intersection and non-intersaction functions

--- array.go
+++ array.go
@@ -15,3 +15,35 @@ func (a *Array) Contains(v any) bool {
 func (a *Array) NotContains(v any) bool {
 	return !a.Contains(v)
 }
+
+func Intersection[T comparable](f, s []T) []T {
+	result := make([]T, 0, len(f))
+	hm := make(map[T]interface{}, len(f))
+	for _, fV := range f {
+		hm[fV] = nil
+	}
+
+	for _, sV := range s {
+		if _, e := hm[sV]; e {
+			result = append(result, sV)
+		}
+	}
+
+	return result
+}
+
+func NonIntersection[T comparable](f, s []T) []T {
+	result := make([]T, 0, len(f))
+	hm := make(map[T]interface{}, len(f))
+	for _, sV := range s {
+		hm[sV] = nil
+	}
+
+	for _, fV := range f {
+		if _, e := hm[fV]; !e {
+			result = append(result, fV)
+		}
+	}
+
+	return result
+}
--- array_test.go
+++ array_test.go
@@ -1,8 +1,9 @@
 package go_basic_extension
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestArray_Contains(t *testing.T) {
@@ -15,4 +16,19 @@ func TestArray_NotContains(t *testing.T) {
 	a := Array{"one", "two"}
 	assert.False(t, a.NotContains("two"))
 	assert.True(t, a.NotContains("three"))
+}
+
+func Test_Intersection(t *testing.T) {
+	first := []int{1, 2, 3, 4, 5, 6}
+	second := []int{3, 6, 10, -1, 12}
+
+	assert.Equal(t, []int{3, 6}, Intersection[int](first, second))
+}
+
+func Test_NonIntersection(t *testing.T) {
+	first := []int{1, 2, 3, 4, 5, 6}
+	second := []int{3, 6, 10, -1, 12}
+
+	assert.Equal(t, []int{1, 2, 4, 5}, NonIntersection[int](first, second))
+	assert.Equal(t, []int{10, -1, 12}, NonIntersection[int](second, first))
 }
